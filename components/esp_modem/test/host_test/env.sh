e0d753d6d4e2d1505d2e2b9985d3c8744a068042
Suren Gabrielyan <suren.gabrielyan@espressif.com>
2023-12-04 16:52:15 +0400

    feat(modem): modem fix host tests

--- components/esp_modem/test/host_test/env.sh
+++ components/esp_modem/test/host_test/env.sh
@@ -3,7 +3,7 @@
 idf_version=$1
 component=$2
 
-if [[ "$idf_version" == "release-v4.3" ]] && [[ "$component" == "esp_modem" ]]; then
+if [[ "$idf_version" == "release-v4.3" ]]; then
   lwip=lwip-2.1.2
   lwip_uri=http://download.savannah.nongnu.org/releases/lwip
   lwip_contrib=contrib-2.1.0
@@ -16,6 +16,22 @@ if [[ "$idf_version" == "release-v4.3" ]] && [[ "$component" == "esp_modem" ]]; 
   apt-get update && apt-get install -y gcc-8 g++-8
   update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 800 --slave /usr/bin/g++ g++ /usr/bin/g++-8
   rm /usr/bin/gcov && ln -s /usr/bin/gcov-8 /usr/bin/gcov
+  export LWIP_PATH=`pwd`/$lwip
+  export LWIP_CONTRIB_PATH=`pwd`/$lwip_contrib
+fi
+
+if [[ "$idf_version" == "release-v4.4" ]]; then
+  lwip=lwip-2.1.2
+  lwip_uri=http://download.savannah.nongnu.org/releases/lwip
+  lwip_contrib=contrib-2.1.0
+
+  apt-get update
+  pip uninstall kconfiglib
+  wget --no-verbose ${lwip_uri}/${lwip}.zip
+  unzip -oq ${lwip}.zip
+  wget --no-verbose ${lwip_uri}/${lwip_contrib}.zip
+  unzip -oq ${lwip_contrib}.zip
+
   export LWIP_PATH=`pwd`/$lwip
   export LWIP_CONTRIB_PATH=`pwd`/$lwip_contrib
 fi
